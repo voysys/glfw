c0b83586634520f85a39ce7c7fa71b2146390419
Torkel <torkel@voysys.se>
2020-10-28 12:37:39 +0900

    response to the shutdown

--- include/GLFW/glfw3.h
+++ include/GLFW/glfw3.h
@@ -1677,10 +1677,11 @@ typedef void (* GLFWwindowclosefun)(GLFWwindow* window);
  *
  *  @sa @ref machine_shutdown
  *  @sa @ref glfwSetMachineShutdownCallback
+ *  @return GLFW_TRUE to accept the shutdown or GLFW_FALSE to interrupt it
  *
  *  @ingroup window
  */
-typedef void (* GLFWmachineShutdownfun)(GLFWwindow*);
+typedef int (* GLFWmachineShutdownfun)(GLFWwindow*);
 
 /*! @brief The function pointer type for window content refresh callbacks.
  *
--- src/win32_window.c
+++ src/win32_window.c
@@ -650,6 +650,11 @@ static LRESULT CALLBACK windowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM l
             return 0;
         }
 
+        case WM_QUERYENDSESSION:
+        {
+            return _glfwInputMachineShutdown(window);
+        }
+
         case WM_INPUTLANGCHANGE:
         {
             _glfwUpdateKeyNamesWin32();
@@ -1324,13 +1329,6 @@ static LRESULT CALLBACK windowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM l
             _glfw_free(paths);
 
             DragFinish(drop);
-            return 0;
-        }
-
-        case WM_QUERYENDSESSION:
-        case WM_ENDSESSION:
-        {
-            _glfwInputMachineShutdown(window);
             return 0;
         }
     }
