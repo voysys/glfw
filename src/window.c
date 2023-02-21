a9cbf0a7f828265010417e4f263913d47f0daec0
Jonathan Nilsson <jonathan@voysys.se>
2023-02-21 16:50:21 +0100

    More fixes

--- src/window.c
+++ src/window.c
@@ -1108,7 +1108,7 @@ GLFWAPI GLFWmachineShutdownfun glfwSetMachineShutdownCallback(GLFWwindow* handle
     assert(window != NULL);
 
     _GLFW_REQUIRE_INIT_OR_RETURN(NULL);
-    _GLFW_SWAP(window->callbacks.shutdown, cbfun);
+    _GLFW_SWAP(GLFWmachineShutdownfun, window->callbacks.shutdown, cbfun);
     return cbfun;
 }
 
